cf06297f3dbdde9a7ce99332b8d7fb49d43ebdf4
duyminh1998 <minhhua12345@gmail.com>
2021-02-09 02:17:25 +0000

    initial commit

---
+++ Module3/a.s
@@ -0,0 +1,17 @@
+.text
+.global main
+
+# Print Message
+main:
+SUB r13, r13, # 4
+STR r14, [r13, #0]
+
+LDR r0, =hw
+BL printf
+
+LDR r14, [r13, #0]
+ADD sp, sp, # 4
+MOV r15, r14
+
+.data
+hw: .asciz "Hello world\n"
