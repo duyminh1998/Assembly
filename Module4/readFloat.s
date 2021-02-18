7be802b935692eea146e8fc10f37c7a2321098ff
duyminh1998 <minhhua12345@gmail.com>
2021-02-18 02:49:31 +0000

    finished hw 4

--- Module4/readFloat.s
+++ Module4/readFloat.s
@@ -15,9 +15,11 @@ ldr r1, =num1
 bl scanf
 
 #Printf
-ldr r0, =output1
 ldr r1, =num1
-ldr r1, [r1, #0]
+vldr s0, [r1]
+vcvt.F64.F32 d4, s0
+ldr r0, =output1
+vmov r1, r2, d4
 bl printf
 
 mov r0, #0
@@ -26,7 +28,7 @@ add sp, sp, #4
 mov pc, lr
 
 .data
+num1: .float 0
 prompt1: .asciz "Enter a floating point number: "
 output1: .asciz "Your floating point number is %f \n"
 input1: .asciz "%f"
-num1: .word 0
