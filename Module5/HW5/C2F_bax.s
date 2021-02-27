82a7394593576f8aff4b576c201fde110b1c2d06
duyminh1998 <minhhua12345@gmail.com>
2021-02-27 08:50:49 +0000

    fixed C2F

--- Module5/HW5/C2F_bax.s
+++ Module5/HW5/C2F_bax.s
@@ -16,21 +16,10 @@ main:
 
 	# we compute C*(9/5) as (C*9*5)/25
 	LDR r0, =num1
-	LDR r0, [r0, #0]
-	MOV r1, #45
-	MUL r4, r0, r1
-	LDR r2, =num2
-	STR r4, [r2, #0]
-	LDR r4, =num2 
-	VLDR s1, [r4]
-	LDR r0, =num3
+	MOV r1, #2
 	VLDR s0, [r0]
-	VDIV.F32 s2, s1, s0
-
-	# Add 32
-	#MOV r1, #32
-	#VLDR s1, [r1, #0]
-	#VADD.F32 s2, s2, s1
+	VLDR s1, [r1]
+	VMUL.F32 s2, s0, s1
 
 	# Print the result
 	LDR r0, =output
@@ -44,8 +33,6 @@ main:
 	MOV pc, lr
 .data
 	num1: .word 0
-	num2: .word 0
-	num3: .word 25
 	prompt: .asciz "Please enter a temperature in degrees C: "
 	input: .asciz "%d"
 	output: .asciz "\n Your temperature in degrees F is %f \n"
