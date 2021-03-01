6e66724498f41e4e207d73f7df2e1ddc95e8a9af
duyminh1998 <minhhua12345@gmail.com>
2021-03-01 02:02:53 +0000

    minor corrections

--- Module5/HW5/swap.s
+++ Module5/HW5/swap.s
@@ -10,12 +10,11 @@ main:
 	# stored in r0
     	MOV r0, #0
     	MOV r1, #1
-    	EOR r2, r0, r1
+	# store value of r1 into r0
+    	EOR r0, r0, r1
 
-	# r2 now contains both r0 and r1
-	# retrieve r1 into r0
-    	EOR r0, r2, r0
-	EOR r1, r2, r1
+	# store value of r0 into r1
+    	EOR r1, r1, r0
 
 	# return to the OS
     	LDR lr, [sp, #0]
